42e6c93af31cb68a35027f5158dedd684674930c
Brett Kelley <mbrotto28@gmail.com>
2021-12-07 12:29:30 +0100

    added rudimentary auth and some features

--- RulePubSubProject/Function.cs
+++ RulePubSubProject/Function.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Cloud.Functions.Framework;
+using Google.Cloud.Functions.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,10 +11,12 @@ namespace RulePubSubProject
 {
     public class Function : IHttpFunction
     {
-        string topicName = "projects/geotab-soleng/topics/BrettTest";
+        private readonly string _topicName = "projects/geotab-soleng/topics/BrettTest";
+        private readonly string _myKey = "keyItem";
+        private readonly string _myKeyValue = "KeyValue";
         private readonly ILogger _logger;
 
-        public Function(ILogger logger)
+        public Function(ILogger<Function> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
@@ -21,24 +24,42 @@ namespace RulePubSubProject
         public async Task HandleAsync(HttpContext context)
         {
             _logger.LogInformation("starting execution...");
+            // get the request object
             HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
             _logger.LogInformation($"ContentType: {request.ContentType}");
-            string message = "";
-            string response = "not found";
+            string topicMessage = "";
             if (request.ContentType.Contains("form"))
             {
                 foreach (string key in request.Form.Keys)
                 {
-                    message += $"key: {key}, value: {request.Form[key]}\n";
-                    _logger.LogInformation(message);
+                    if (string.Equals(key, _myKey))
+                    {
+                        if (string.Equals(request.Form[key], _myKeyValue))
+                        {
+                            _logger.LogInformation($"Successfully authenticated...");
+                            response.StatusCode = StatusCodes.Status200OK;
+                        }
+                        else
+                        {
+                            response.StatusCode = StatusCodes.Status401Unauthorized;
+                        }
+                    }
+                    else
+                    {
+                        topicMessage += $"key: {key}, value: {request.Form[key]}\n";
+                        _logger.LogInformation(topicMessage);
+                    }
                 }
             }
-            _logger.LogInformation("publishing message to topic...");
+            //_logger.LogInformation("publishing message to topic...");
             //await _pubSub.SendMessageAsync(topicName, message);
 
             _logger.LogInformation("finishing execution...");
-            _logger.LogDebug($"response: {response}");
-            await context.Response.WriteAsync($"{response}");
+
+            //await context.Response.WriteAsync($"{responseMessage}");
+            await response.CompleteAsync();
         }
     }
 }
